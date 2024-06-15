b241222bbfe80eaa66addf7fd21d2514213795fb
seanja <sean.sandy@gmail.com>
2024-06-15 11:05:17 -0400

    should be capitalized

--- src/ValueObjects/VectorDataPoints/VectorDataPoints.php
+++ src/ValueObjects/VectorDataPoints/VectorDataPoints.php
@@ -10,12 +10,12 @@ class VectorDataPoints extends AbstractCollection implements Deserializable
     public static function deserialize(array $data): static
     {
         return new static(array_map(function ($data) {
-            return VectorDatapoint::deserialize($data);
+            return VectorDataPoint::deserialize($data);
         }, $data));
     }
 
     #[\Override] public function getType(): string
     {
-        return VectorDatapoint::class;
+        return VectorDataPoint::class;
     }
 }
